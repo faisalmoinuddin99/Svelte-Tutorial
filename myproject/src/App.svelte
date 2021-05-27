889592333625f2c716b8ff947a4e4f7c65818bdf
faisal <faisal25march99@gmail.com>
2021-05-27 20:34:32 +0530

    Loops in Svelte

--- myproject/src/App.svelte
+++ myproject/src/App.svelte
@@ -1,46 +1,34 @@
 <script>
-  let name = "naruto";
-  let beltColor = "black";
-  let first_name = "Jimmy";
-  let last_name = "sharma";
-
-  // Reactive Value
-  $: full_name = `${first_name} ${last_name}`;
-
-  // Reactive Statement
-  $: console.log(beltColor);
-
-  $: {
-    console.log(beltColor);
-    console.log(full_name);
-  }
-
-  let handleClick = () => {
-    if (beltColor == "black") {
-      beltColor = "yellow";
-    } else {
-      beltColor = "black";
-    }
-  };
-
-  let handleInput = (e) => {
-    beltColor = e.target.value;
-  };
+  let peope = [
+    { id: 1, name: "Naruto", age: 20, beltcolor: "black" },
+    { id: 2, name: "Saske", age: 21, beltcolor: "orange" },
+    { id: 3, name: "Sakura", age: 20, beltcolor: "pink" },
+  ];
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>{full_name} - {beltColor} belt</p>
-
-  <button on:click={handleClick}>update belt color</button>
-
-  <!-- one-way data binding -->
-  <input type="text" on:input={handleInput} value={beltColor} />
-
-  <hr />
-
-  <input type="text" bind:value={first_name} />
-  <input type="text" bind:value={last_name} />
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">NAME</th>
+        <th scope="col">AGE</th>
+        <th scope="col">BELT-COLOR</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each peope as ninja (ninja.id)}
+        <tr>
+          <th scope="row">{ninja.id}</th>
+          <td>{ninja.name}</td>
+          <td>{ninja.age}</td>
+          <td>{ninja.beltcolor}</td>
+        </tr>
+      {:else}
+        <p>There are no ninja to show...</p>
+      {/each}
+    </tbody>
+  </table>
 </main>
 
 <style>
@@ -49,13 +37,6 @@
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
   }
 
   @media (min-width: 640px) {
