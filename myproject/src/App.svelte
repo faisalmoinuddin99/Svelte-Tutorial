aa81610f67706a6988b08e0bc9e8b8782bf153f6
faisal <faisal25march99@gmail.com>
2021-05-29 04:34:32 +0530

    Components

--- myproject/src/App.svelte
+++ myproject/src/App.svelte
@@ -1,67 +1,9 @@
 <script>
-  let people = [
-    { id: 1, name: "Naruto", age: 20, beltcolor: "black" },
-    { id: 2, name: "Saske", age: 21, beltcolor: "orange" },
-    { id: 3, name: "Sakura", age: 20, beltcolor: "pink" },
-  ];
-
-  const handleClick = (e, id) => {
-    console.log(id);
-    people = people.filter((ninjas) => ninjas.id != id);
-    console.log(e);
-  };
+  import Modal from "./components/Modal.svelte";
 </script>
 
 <main>
-  <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">ID</th>
-        <th scope="col">NAME</th>
-        <th scope="col">AGE</th>
-        <th scope="col">BELT-COLOR</th>
-        <th scope="col">CATEGORY</th>
-      </tr>
-    </thead>
-    <tbody>
-      {#each people as ninja (ninja.id)}
-        <tr>
-          <th scope="row">{ninja.id}</th>
-          <td>{ninja.name}</td>
-          <td>{ninja.age}</td>
-          <td>{ninja.beltcolor}</td>
-          <td>
-            {#if ninja.beltcolor === "black"}
-              <p><strong>MASTER NINJA</strong></p>
-            {:else}
-              <p>N.A</p>
-            {/if}
-          </td>
-          <!-- inline Event Handler -->
-          <button
-            on:click={(e) => {
-              handleClick(e, ninja.id);
-            }}>delete</button
-          >
-        </tr>
-      {:else}
-        <p>There are no ninja to show...</p>
-      {/each}
-    </tbody>
-  </table>
+  <Modal />
 </main>
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
+<style></style>
---
+++ myproject/src/components/Modal.svelte
@@ -0,0 +1,13 @@
+<script>
+  let showModal = true;
+</script>
+
+{#if showModal === true}
+  <div class="backdrop">
+    <div class="modal">
+      <p>I am Modal</p>
+    </div>
+  </div>
+{/if}
+
+<style></style>
