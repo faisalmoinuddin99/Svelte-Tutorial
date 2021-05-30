792280b5990952f9685069cfba3e93685f72b3a5
faisal <faisal25march99@gmail.com>
2021-05-30 12:13:58 +0530

    Event Modifiers

--- myproject/src/App.svelte
+++ myproject/src/App.svelte
@@ -11,9 +11,15 @@
 <main>
   <!-- <Modal message="Hey, I am a prop value" isPromo={true} /> -->
   <Modal message="I am another modal" {showModal} on:click={toggleModal} />
-  <button on:click={toggleModal}> Show Modal</button>
+  <button on:click|once={toggleModal}> Show Modal</button>
   <h1>Hello world</h1>
 </main>
 
 <style>
+  button {
+    background-color: lightblue;
+    color: black;
+    font-size: 25px;
+    margin: auto;
+  }
 </style>
--- myproject/src/components/Modal.svelte
+++ myproject/src/components/Modal.svelte
@@ -2,11 +2,18 @@
   export let showModal;
   export let isPromo;
   export let message = "default";
+
+  /*
+  * Event Modifier: 
+  1. once - make sure the event can only fire once (remove handler)
+  2. preventDefault - prevent the default action (run e.preventDefault())
+  3. self - only fires the event if the clicked element is the target
+  */
 </script>
 
 {#if showModal === true}
   <!-- Event Forwarding -->
-  <div class="backdrop" class:promo={isPromo} on:click>
+  <div class="backdrop" class:promo={isPromo} on:click|self>
     <div class="modal">
       <p>{message}</p>
     </div>
